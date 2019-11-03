679829e68d873ca256751bd76b6e8391c7722f92
Gustavo Magnago <gusmagnago@gmail.com>
2019-11-03 20:43:15 +0000

    all the places are rendering in the search view

--- routes/index.js
+++ routes/index.js
@@ -8,32 +8,16 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-// ĈREATE
-router.get("/create", (req, res, next) => {
-  res.render("create");
-});
-
-router.post("/create", (req, res, next) => {
-  const name = req.body.name;
-  const address = req.body.address;
-  const latitude = req.body.latitude;
-  const longitude = req.body.longitude;
-
-  Places.create({
-    name,
-    address,
-    latitude,
-    longitude
-  })
+// FIND
+router.get("/search", (req, res, next) => {
+  Places.find()
     .then(places => {
-      res.redirect("/create");
-      console.log("a place was created", places);
+      res.render("search", { places });
     })
     .catch(error => {
-      console.log("an error occuried trying to create a place", error);
+      console.log(error);
     });
 });
-// FIND
 
 // SEARCH
 router.get("/search", (req, res, next) => {
